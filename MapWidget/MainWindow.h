5ecd6eaca7230a29e6dd5eacacaab33bbe668a22
heganyu <1206114928@qq.com>
2018-03-22 20:55:17 +0800

    增加地图查看例程

---
+++ MapWidget/MainWindow.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma execution_character_set("utf-8")
+#include <QtWidgets/QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	MainWindow(QWidget *parent = Q_NULLPTR);
+
+private:
+};
